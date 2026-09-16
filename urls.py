69d3a4835d09f2281929c624dbe553485c94c8d2
skkq <skkqw@yandex.ru>
2026-09-16 20:53:30 +0300

    Добавление новых urls

--- urls.py
+++ urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('apps.users.api.urls')),
 ]
+
+# Документация только для dev режима.
+if settings.DEBUG:
+    from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    ]
